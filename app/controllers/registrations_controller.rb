b2a958585f94a8ef069131ec33f0333d7cafce9e
MamtaBobal <mamta.cs.du.2013@gmail.com>
2019-12-14 15:24:29 +0530

    Link Secret Code with User

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new(user_params)
     if user.save
+      @secret_code.update(user_id: user.id)
       redirect_to new_user_session_path
     else
       redirect_to new_user_registration_path, status: :unprocessable_entity
@@ -13,8 +14,8 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def validate_secret_code
-    if params[:secret_code].present? && params[:secret_code] == 'foobar'
-    else
+    @secret_code = SecretCode.find_by(code: params[:secret_code])
+    unless @secret_code && @secret_code.user_id.nil?
       redirect_to new_user_registration_path, flash: { error: "Invalid Secret Code" }
     end
   end
